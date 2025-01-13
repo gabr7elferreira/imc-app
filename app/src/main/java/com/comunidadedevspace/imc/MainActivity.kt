c6eae8e6c53e1d5cff791b0b9b80e0545cab93db
Gabr7el Ferreira <gabrielferreira.dev@icloud.com>
2025-01-12 21:44:45 -0800

    Capturando EditText e Button na tela inicial do App

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,30 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recupear os componetes EditText
+        //criar uma variavel e associar ao componente de UI<editText>
+        //recuperar bottun da tela
+        //coloar acao no button
+        //recuperar texto digitado no peso
+
+
+        val edt_peso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edt_altura = findViewById<TextInputEditText>(R.id.edt_altura)
+        val btn_calcular = findViewById<Button>(R.id.btn_calcular)
+
+        btn_calcular.setOnClickListener {
+            val peso = edt_peso.text
+            val altura = edt_altura.text
+            //val media: Double = peso * altura
+
+        }
     }
 }
